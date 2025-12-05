c03a07ef60a837cc4f620d39303086a8669bce49
leonid irinceev <Kromanyons@mail.ru>
2025-12-05 22:19:05 +0700

    Add files via upload

---
+++ UNIX 1/hi.cpp
@@ -0,0 +1,6 @@
+// Output: my_program
+#include <iostream>
+int main() {
+    std::cout << "giant cockroache" << std::endl;
+    return 0;
+}
---
+++ UNIX 1/lab1.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+#1. Возвращать информативный код ошибки 
+error_exit() {
+    echo "ОШИБКА: $1" >&2
+    exit 1
+}
+
+# Для очистки временного каталога
+cleanup() {
+    if [ -n "$TEMP_DIR" ] && [ -d "$TEMP_DIR" ]; then
+        rm -rf "$TEMP_DIR"
+    fi
+}
+
+# Проверка количества аргументов
+if [ "$#" -ne 1 ]; then
+    error_exit "После $0 должен передаваться 1 аргумент"  
+fi
+
+# Проверка на существование файла
+SOURCE_FILE="$1"
+SOURCE_DIR=$(dirname "$SOURCE_FILE")
+SOURCE_FILENAME=$(basename "$SOURCE_FILE")
+if [ ! -f "$SOURCE_FILE" ]; then
+    error_exit "Исходный файл не найден/недоступен для чтения: $SOURCE_FILE"
+fi
+
+# Проверка расширения файла
+if [ "${SOURCE_FILENAME##*.}" != "cpp" ]; then
+    error_exit "Поддерживаются только файлы с расширением .cpp: $SOURCE_FILE"
+fi
+
+# 2. Анализ текста и поиск комментария с именем выходного файла
+OUTPUT_FILENAME=$(grep 'Output:' "$SOURCE_FILE" | sed 's/.*Output:[[:space:]]*//')
+
+# Проверка на коммент(-z на пустоту переменной)
+if [ -z "$OUTPUT_FILENAME" ]; then
+    error_exit "Не удалось найти комментарий с именем выходного файла в формате 'Output: <имя_файла>'."
+fi
+
+# 3. Сборка должна производиться в временном каталоге с  mktemp, 
+TEMP_DIR=$(mktemp -d XXXXXXX)
+
+# 4. Каталог должен быть удалён при любом исходе работы скрипта
+trap cleanup EXIT HUP INT TERM
+
+# Компиляция C++-файла
+echo "Компиляция C++ файла..."
+if g++ -o "$TEMP_DIR/$OUTPUT_FILENAME" "$SOURCE_FILE"; then
+    COMPILATION_SUCCESSFUL=true
+else
+    error_exit "Ошибка компиляции"
+fi
+
+# Проверка наличия скомпилированного файла
+if [ "$COMPILATION_SUCCESSFUL" = true ]; then
+    if [ -f "$TEMP_DIR/$OUTPUT_FILENAME" ]; then
+        mv "$TEMP_DIR/$OUTPUT_FILENAME" "$SOURCE_DIR/"
+        echo "Компиляция окончена. Результат: $SOURCE_DIR/$OUTPUT_FILENAME"
+    else
+        error_exit "Скомпилированный файл не найден в temp каталоге."
+    fi
+fi
+
+exit 0
