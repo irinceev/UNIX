311a1f729bc1ff3f35ad7b5a0ef681a06ef42bba
leonid irinceev <Kromanyons@mail.ru>
2025-12-17 22:52:41 +0700

    Update lab3.c

--- OS 3/lab3.c
+++ OS 3/lab3.c
@@ -5,18 +5,20 @@
 #include <linux/seq_file.h>
 #include <linux/time.h>
 
-#define PROC_NAME "corelab"
+#define PROC_NAME "tsulab"
 
 static int corelab_proc_show(struct seq_file *m, void *v)
 {
-     /* Параметры полёта Восток-6:
-     Запуск Восток-6: 16 июня 1963 года 09:29:52 UTC
-     Реально витков совершила Терешкова: 48 витков
-     Орбитальный период Восток-6: 88.3 минуты */
+    /*
+     * Параметры полёта Восток-6:
+     * Запуск Восток-6: 16 июня 1963 года 09:29:52 UTC
+     * Реально витков совершила Терешкова: 48 витков
+     * Орбитальный период Восток-6: 88.3 минуты
+     */
 
-    const time64_t start_ts = -206548208; // заранее посчитано date -u -d '1963-06-16 09:29:52' +%s
-    const time64_t orbit_period_sec = 88 * 60 + 18; // 88 минуты 18 сек - примерно 88.3 мин 
-    const long long real_orbits = 48; // фактическое число витков
+    const time64_t start_ts = -206548208; /* заранее посчитано date -u -d '1963-06-16 09:29:52' +%s*/
+    const time64_t orbit_period_sec = 88 * 60 + 18; /* 88 минуты 18 сек - примерно 88.3 мин */
+    const long long real_orbits = 48; /* фактическое число витков */
 
     time64_t now = ktime_get_real_seconds();
     long long total_orbits = 0;
@@ -25,7 +27,7 @@ static int corelab_proc_show(struct seq_file *m, void *v)
     if (now > start_ts) {
         time64_t dt = now - start_ts;
         total_orbits = dt / orbit_period_sec;
-        if (total_orbits > real_orbits) //ну мало ли
+        if (total_orbits > real_orbits)
             extra_orbits = total_orbits - real_orbits;
         else
             extra_orbits = 0;
