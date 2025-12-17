e7e6cd0523256f16dcfe4735be7cbc0709982cee
leonid irinceev <Kromanyons@mail.ru>
2025-12-17 22:36:41 +0700

    Update lab3.c

--- OS 3/lab3.c
+++ OS 3/lab3.c
@@ -5,25 +5,19 @@
 #include <linux/seq_file.h>
 #include <linux/time.h>
 
-#define PROC_NAME "tsulab"
+#define PROC_NAME "corelab"
 
-
-
-static int tsulab_proc_show(struct seq_file *m, void *v)
+static int corelab_proc_show(struct seq_file *m, void *v)
 {
     /*
      * Параметры полёта Восток-6:
-     * старт: 16 июня 1963 года 09:29:52 UTC
-     * реальная длительность: ~48 витков
-     * орбитальный период: ~88.3 минуты
+     * Запуск Восток-6: 16 июня 1963 года 09:29:52 UTC
+     * Реально витков совершила Терешкова: 48 витков
+     * Орбитальный период Восток-6: 88.3 минуты
      */
 
-    /* время старта Восток-6 (1963-06-16 09:29:52 UTC) */
-    const time64_t start_ts = -185450408; /* заранее посчитанный timestamp */
-
-    /* длительность одного витка в секундах (~88.3 мин) */
-    const time64_t orbit_period_sec = 88 * 60 + 18; /* 88 мин 18 сек ≈ 88.3 мин */
-
+    const time64_t start_ts = -206548208; /* заранее посчитано date -u -d '1963-06-16 09:29:52' +%s*/
+    const time64_t orbit_period_sec = 88 * 60 + 18; /* 88 минуты 18 сек - примерно 88.3 мин */
     const long long real_orbits = 48; /* фактическое число витков */
 
     time64_t now = ktime_get_real_seconds();
@@ -50,34 +44,34 @@ static int tsulab_proc_show(struct seq_file *m, void *v)
     return 0;
 }
 
-static int tsulab_proc_open(struct inode *inode, struct file *file)
+static int corelab_proc_open(struct inode *inode, struct file *file)
 {
-    return single_open(file, tsulab_proc_show, NULL);
+    return single_open(file, corelab_proc_show, NULL);
 }
 
-static const struct proc_ops tsulab_proc_ops = {
-    .proc_open    = tsulab_proc_open,
+static const struct proc_ops corelab_proc_ops = {
+    .proc_open    = corelab_proc_open,
     .proc_read    = seq_read,
     .proc_lseek   = seq_lseek,
     .proc_release = single_release,
 };
 
-static int __init tsulab_init(void)
+static int __init corelab_init(void)
 {
-    if (!proc_create(PROC_NAME, 0444, NULL, &tsulab_proc_ops))
+    if (!proc_create(PROC_NAME, 0444, NULL, &corelab_proc_ops))
         return -ENOMEM;
 
     pr_info("Welcome to the Tomsk State University\n");
     return 0;
 }
 
-static void __exit tsulab_exit(void)
+static void __exit corelab_exit(void)
 {
     remove_proc_entry(PROC_NAME, NULL);
     pr_info("Tomsk State University forever!\n");
 }
 
-module_init(tsulab_init);
-module_exit(tsulab_exit);
+module_init(corelab_init);
+module_exit(corelab_exit);
 
 MODULE_LICENSE("GPL");
